ed15cf40871c1e11412c7abf3354e883dc816848
p-k-pavan <pkpavan2003@email.com>
2024-03-31 18:39:36 +0530

    create listing model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose")
+const Listing = require("./models/listing.js")
 const app = express();
 
 main().then(()=>{
@@ -11,6 +12,18 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wounderLust');
 
 }
+
+app.get("/testListing",async (req,res)=>{
+    let newListing = new Listing({
+        title:"villa",
+        discription:"mud",
+        price:500,
+        country:"India",
+        location:"South india"
+    })
+    await newListing.save();
+    res.send("Sucess full")
+})
 
 const PORT = 3000;
 
---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    discription:{
+        type:String
+    },
+    image:{
+        type:String,
+        default:"https://images.unsplash.com/photo-1562692813-9f7ec308836a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    country:{
+        type:String,
+        require:true
+    },
+    location:{
+        type:String,
+        require:true
+    }
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
