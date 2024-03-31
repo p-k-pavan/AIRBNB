5a8d890d1f881913257912df9c8c2406cde65525
p-k-pavan <pkpavan2003@email.com>
2024-03-31 19:59:06 +0530

    index routing

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js")
+const path = require("path")
+
 const app = express();
 
 
-app.get("/testListing",async (req,res)=>{
-    let newListing = new Listing({
-        title:"villa",
-        discription:"mud",
-        price:500,
-        country:"India",
-        location:"South india"
-    })
-    await newListing.save();
-    res.send("Sucess full")
+main().then(()=>{
+    console.log("connected db")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wounderLust');
+
+}
+
+// app.get("/testListing",async (req,res)=>{
+//     let newListing = new Listing({
+//         title:"villa",
+//         discription:"mud",
+//         price:500,
+//         country:"India",
+//         location:"South india"
+//     })
+//     await newListing.save();
+//     res.send("Sucess full")
+// })
+
+app.set("view engine","ejs")
+app.set("views" , path.join(__dirname,"views"))
+
+app.get("/listings",async(req,res)=>{
+    const allListing = await Listing.find({})
+    res.render("listings/index.ejs",{allListing})
 })
 
 const PORT = 3000;
 
-app.listen(PORT,(req,res)=>{
+app.listen(PORT,()=>{
     console.log("Server was listen in port ",PORT)
 })
