0101d7a5970749842ea1c885203eb7cfd2a956ec
p-k-pavan <pkpavan2003@email.com>
2024-04-02 23:26:16 +0530

    update(edit and update route)

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
 const express = require("express");
-const mongoose = require("mongoose")
-const Listing = require("./models/listing.js")
-const path = require("path")
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const app = express();
 
-
-main().then(()=>{
-    console.log("connected db")
-})
-.catch(err => console.log(err));
+main()
+  .then(() => {
+    console.log("connected db");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wounderLust');
-
+  await mongoose.connect("mongodb://127.0.0.1:27017/wounderLust");
 }
 
-// app.get("/testListing",async (req,res)=>{
-//     let newListing = new Listing({
-//         title:"villa",
-//         discription:"mud",
-//         price:500,
-//         country:"India",
-//         location:"South india"
-//     })
-//     await newListing.save();
-//     res.send("Sucess full")
-// })
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.set("view engine","ejs")
-app.set("views" , path.join(__dirname,"views"))
-app.use(express.urlencoded({extended:true}))
-
-app.get("/listings",async(req,res)=>{
-    const allListing = await Listing.find({})
-    res.render("listings/index.ejs",{allListing})
-})
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
+});
 
 // new listings
 
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
-})
-
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
 //show route
 
 app.get("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    
-        let data = await Listing.findById(id);
-       
-        res.render("listings/show.ejs", { data });
-    
+  let { id } = req.params;
+
+  let data = await Listing.findById(id);
+
+  res.render("listings/show.ejs", { data });
 });
 
-//create route 
- app.post("/listings",async(req,res)=>{
-    let newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings")
- })
+//create route
+app.post("/listings", async (req, res) => {
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
+//edit route
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let id = req.params.id;
+  const data = await Listing.findById(id);
+  res.render("listings/edit.ejs", { data });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let id = req.params.id;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log("Server was listen in port ",PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server was listen in port ", PORT);
+});
--- init/index.js
+++ init/index.js
@@ -1,23 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const data = require("./data.js");
-const Listing = require("../models/listing.js")
+const Listing = require("../models/listing.js");
 
-
-
-main().then(()=>{
-    console.log("connected db")
-})
-.catch(err => console.log(err));
+main()
+  .then(() => {
+    console.log("connected db");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wounderLust');
-
+  await mongoose.connect("mongodb://127.0.0.1:27017/wounderLust");
 }
 
-const initDB = async ()=>{
-     await Listing.deleteMany({})
-     await Listing.insertMany(data.data)
-     console.log("Data was initialized success")
-}
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(data.data);
+  console.log("Data was initialized success");
+};
 
 initDB();
