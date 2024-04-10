d5ea7c775c8558b920ecefa68fe811b4f46dfb1f
p-k-pavan <pkpavan2003@email.com>
2024-04-10 17:37:20 +0530

    validate for schema

--- Utils/ExpressError.js
+++ Utils/ExpressError.js
@@ -1,8 +1,8 @@
-class ExpressError extends Error{
-    constructor(statusCode,message){
+class ExpressError extends Error {
+    constructor(statusCode, message) {
         super();
-        this.statusCode=statusCode;
-        this.message=message;
+        this.statusCode = statusCode;
+        this.message = message;
     }
 }
 
--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const WrapAsync = require("./Utils/WrapAsync.js")
-const ExpressError = require("./Utils/ExpressError.js")
+const WrapAsync = require("./Utils/WrapAsync.js");
+const ExpressError = require("./Utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const app = express();
 
@@ -30,16 +31,25 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+};
 
 // Routes
 
 // Index route - Display all listings
-app.get("/listings", WrapAsync(async (req, res) => {
-
+app.get(
+  "/listings",
+  WrapAsync(async (req, res) => {
     const allListing = await Listing.find({});
     res.render("listings/index", { allListing });
- 
-}));
+  })
+);
 
 // New listing form
 app.get("/listings/new", (req, res) => {
@@ -47,85 +57,73 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Show route - Display details of a specific listing
-app.get("/listings/:id", WrapAsync(async (req, res) => {
- 
+app.get(
+  "/listings/:id",
+  WrapAsync(async (req, res) => {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
       return res.status(404).send("Listing not found");
     }
     res.render("listings/show", { listing });
- 
-}));
+  })
+);
 
 // Create route - Add a new listing
 // Create route - Add a new listing
-app.post("/listings", WrapAsync(async (req, res, next) => {
-  const newListing = new Listing(req.body.listing);
-  try {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Enter valid data for Listing!");
-    }
+app.post(
+  "/listings",
+  validateListing,
+  WrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-  } catch (error) {
-    next(error); // Pass the error to the error handling middleware
-  }
-}));
-
+  })
+);
 
 // Edit route - Display form to edit a listing
-app.get("/listings/:id/edit", WrapAsync(async (req, res) => {
- 
+app.get(
+  "/listings/:id/edit",
+  WrapAsync(async (req, res) => {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
       return res.status(404).send("Listing not found");
     }
     res.render("listings/edit", { listing });
-  
-}));
+  })
+);
 
 // Update route - Update a listing
-app.put("/listings/:id", WrapAsync(async (req, res, next) => {
-  try {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Enter valid data for Listing!");
-    }
-  
+app.put(
+  "/listings/:id",
+  validateListing,
+  WrapAsync(async (req, res, next) => {
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
     res.redirect(`/listings/${id}`);
-  } catch (error) {
-    next(error); // Pass the error to the error handling middleware
-  }
-}));
+  })
+);
 
 // Delete route - Delete a listing
-app.delete("/listings/:id", WrapAsync(async (req, res) => {
-
+app.delete(
+  "/listings/:id",
+  WrapAsync(async (req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
- 
-}));
+  })
+);
 
-app.all("*",(req,res,next)=>{
-  next(new ExpressError(404,"Page not found"))   
-})
-
-app.use((err, req, res, next) => {
-  let statusCode = err.statusCode || 500; 
-  let message = err.message || "Internal Server Error"; 
-  
-  // Handling specific error types
-  if (err instanceof ExpressError) {
-    statusCode = err.statusCode;
-    message = err.message;
-  }
-
-  // res.status(statusCode).send(message);
-  res.render("listings/Error",{message})
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found"));
 });
 
+app.use((err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  // res.status(statusCode).send(message);
+  res.render("listings/Error", { message });
+});
 
 const PORT = process.env.PORT || 3000;
 
