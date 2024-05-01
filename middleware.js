afd77cce2023905e07f60ddc1e9f124ce9313aac
p-k-pavan <pkpavan2003@email.com>
2024-05-01 12:01:57 +0530

    post login page

--- middleware.js
+++ middleware.js
@@ -2,8 +2,19 @@ module.exports.isLoggedIn = (req,res,next)=>{
   console.log(req.user);
 
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error","You must login before creating listing")
         return res.redirect("/login")
+      }
+      next();
+}
+
+module.exports.saveRedirectUrl = (req,res,next)=>{
+  console.log(req.user);
+
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl
+       
       }
       next();
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const ExpressError = require("../Utils/ExpressError.js");
 const WrapAsync = require("../Utils/WrapAsync.js");
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signUp", (req, res) => {
   res.render("User/signUp.ejs");
@@ -17,8 +18,14 @@ router.post(
 
       const newUser = new User({ email, username });
       const userRegister = await User.register(newUser, password);
-      req.flash("success", "user register successfully!");
-      res.redirect("/login");
+      req.login(userRegister,(err)=>{
+        if(err){
+          next(err);
+        }
+        req.flash("success", "user register successfully!");
+      res.redirect("/listings");
+      })
+      
     } catch (error) {
       req.flash("success", "user register alredy exist");
       res.redirect("/signUp");
@@ -32,15 +39,17 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
-    res.redirect("/listings");
+    res.redirect(res.locals.redirectUrl); // Corrected from req.locals to res.locals
     req.flash("success", "user Login successfully!");
   }
 );
+
 
 router.get("/logOut",(req,res,next)=>{
   req.logout((err)=>{
